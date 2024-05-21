b0d7d0d04e9fbf614c688c5f0c31ce97bfceecf6
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-05-21 18:36:14 +0530

    setup & requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.1',
+    author = 'Harshal Kumre',
+    author_email = 'kumreharshalkumar@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
